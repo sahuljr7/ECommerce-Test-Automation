c3c3f7690835fea993f9e6184b08341fdff11216
Sahul Kumar Parida <71061738+sahuljr7@users.noreply.github.com>
2025-06-14 11:40:25 +0530

    Create AppConstants.java

---
+++ AppConstants.java
@@ -0,0 +1,14 @@
+package com.ecommerce.constants;
+
+public class AppConstants {
+    public static final int DEFAULT_SHORT_TIME_OUT = 5;
+    public static final int DEFAULT_MEDIUM_TIME_OUT = 10;
+    public static final int DEFAULT_LONG_TIME_OUT = 20;
+    
+    public static final String LOGIN_PAGE_TITLE = "Login - My Store";
+    public static final String HOME_PAGE_TITLE = "My Store";
+    public static final String CART_PAGE_TITLE = "Order - My Store";
+    
+    public static final String TEST_DATA_SHEET_PATH = "./src/main/resources/testdata/testdata.xlsx";
+    public static final String TEST_DATA_JSON_PATH = "./src/main/resources/testdata/testdata.json";
+}
